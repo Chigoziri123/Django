dc6da65a9ffccf41c30fff0039ff6598f013a34a
Chigoziri123 <anuna.s@bullseyemanpower.com>
2023-05-24 22:05:48 +0100

    linked details.html file

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('users/', views.users, name='users'),
+    path('users/details/<int:id>', views.details, name='details')
 ]
--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CUSTOM_APPS = [
+'article',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+] + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
